8878172e4a55c60b6ab9e45dc953ba3704c15afd
Franklinson <franklinhassey@gmail.com>
2025-09-18 00:40:08 +0000

    feat: Register all LMS models with Django admin

--- lms_app/admin.py
+++ lms_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Course, Lesson, Quiz, Question, Answer, Enrollment, LessonProgress, QuizAttempt
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Enrollment)
+admin.site.register(LessonProgress)
+admin.site.register(QuizAttempt)
